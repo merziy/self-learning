c20a917a5abe2a622ae4edc3ef23ea37872c9d99
merziy <alexzandermathis@gmail.com>
2022-01-06 13:52:52 -0500

    Started FCC React tutorial

---
+++ React/fcc-react-video/tutorial/src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https:/images-na.ssl-images-amazon.com/images/I/
+81eB%2B7%2BCkUL._AC_UL200_SR200_,200_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
+    Amelia Hepworth
+  </h4>
+);
+
+ReactDom.render(<BookList />, document.getElementById("root"));
