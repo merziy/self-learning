2dcfecaebf15ab10e38ee1353aa4ebcc912320a8
merziy <alexzandermathis@gmail.com>
2022-02-05 11:49:04 -0500

    Added Python web crawler project

---
+++ python/web-crawler/README.md
@@ -0,0 +1,3 @@
+# Python Web Crawlers
+
+There are two web crawlers that I construced following [this](freecodecamp.org/news/webscraping-in-python/) article. One crawls .html from URLs that are saved in a local .txt file, the other crawls from an Angular SPA and uses the Selenium library.
---
+++ python/web-crawler/crawler.py
@@ -0,0 +1,50 @@
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from wordcloud import WordCloud
+
+
+def extract(url):
+    elem = None
+    driver = webdriver.Firefox()
+    driver.get(url)
+
+    try:
+        found = WebDriverWait(driver, 10).until(
+            EC.visibility_of(
+                driver.find_element(By.TAG_NAME, "article")
+            )
+        )
+        # Make a copy of relevant data, because Selenium will throw if
+        # you try to access the properties after the driver quit
+        elemt = {
+            "text": found.text
+        }
+    finally:
+        driver.close()
+
+    return elem
+
+
+def transform(elem):
+    return elem["text"]
+
+
+def load(text, filepath):
+    cloud = WordCloud().generate(text)
+    cloud.to_file(filepath)
+
+
+if __name__ == '__main__':
+    url = "https://angular.io/"
+    filepath = "angular.png"
+
+    elem = extract(url)
+    if elem is not None:
+        text = transform(elem)
+        load(text, filepath)
+    else:
+        print("Sorry, could not extract data")
