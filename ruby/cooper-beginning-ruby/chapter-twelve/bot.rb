c3efc873506a7aed251393c74d4a7a3ab409405a
Alexander Mathis <alexzandermathis@gmail.com>
2024-06-12 07:43:54 -0400

    chatbot skeleton

---
+++ ruby/cooper-beginning-ruby/chapter-twelve/bot.rb
@@ -0,0 +1,88 @@
+require 'yaml'
+require_relative 'wordplay'
+
+# A chatterbot
+class Bot
+  attr_reader :name
+
+  def initialize(options)
+    @name = options[:name] || "Unnamed Bot"
+    begin
+      @data = YAML.load_file(options[:data_file])
+    rescue
+      raise "Can't load bot data"
+    end
+  end
+
+  def greeting
+    random_response(:greeting)
+  end
+
+  def farewell
+    random_response(:farewell)
+  end
+
+  def response_to(input)
+    prepared_input = preprocess(input.downcase)
+    sentence = best_sentence(prepared_input)
+    responses = possible_responses(sentence)
+    responses[rand(responses.length)]
+  end
+
+  private
+
+  def random_response(key)
+    random_index = rand(@data[:responses][key].length)
+    @data[:responses][key][random_index].gsub(/\[name\]/, @name)
+  end
+
+
+  def preprocess(input)
+    peform_substitutions input
+  end
+
+  def peform_substitutions(input)
+    @data[:presubs].each { |s| input.gsub!(s[0], s[1]) }
+    input
+  end
+
+  def best_sentence(input)
+    hot_words = @data[:responses].keys.select do |k|
+      k.class == String && k =~ /^\w+$/
+    end
+
+    WordPlay.best_sentence(input.sentences, hot_words)
+  end
+
+  # Find all patterns to try to match against
+  def possible_responses(sentence)
+    responses = []
+
+    # For each pattern, see if the supplied sentence contains
+    # a match. Remove substitution symbols (*) before checking.
+    # Push all responses to the responses array.
+    @data[:responses].keys.each do |pattern|
+      next unless pattern.is_a?(String)
+
+      if sentence.match('\b' + pattern.gsub(/\*/, '') + '\b')
+        # If the patern constains substitution placeholders, peform the substitution
+        if pattern.include?('*')
+          responses << @data[:responses][pattern].collect do |phrase|
+            # First, erase everything before the placeholder, with the pronouns switched
+            matching_selection = sentence.sub(/^.*#{pattern}\s/, '')
+            # Then, substitute the text after the placeholder with the pronouns switched
+            phrase.sub('*', WordPlay.switch_pronouns(matching_selection))
+          end
+        else
+          # No placeholders? Add the phrase to the array
+          responses << @data[:responses][pattern]
+        end
+      end
+    end
+
+    # If there were no matches, add the default ones.
+    responses << @data[:responses][:default] if responses.empty?
+    # Flattern the blocks of sentences to a flat array
+    responses.flatten
+  end
+end
---
+++ ruby/cooper-beginning-ruby/chapter-twelve/test_wordplay.rb
@@ -0,0 +1,50 @@
+require 'test/unit'
+require 'wordplay'
+
+class TestWordPlay < Test::Unit::TestCase
+  def test_sentences
+    assert_equal(["a", "b", "c d", "e f g"], "a. b. c d. e f g.".sentences)
+
+    test_text = %q{Hello. This is a Test
+      of sentence seperation. This is the end
+      of the test. }
+    assert_equal("This is the end of test", test_text.sentences[2])
+  end
+
+  def test_words
+    assert_equal(%w{this is a test}, "this is a test".words)
+    assert_equal(%w{these are mostly words}, "these are, mostly, words".words)
+  end
+
+  def test_best_sentence_choice
+    assert_equal('This is a great test',
+      WordPlay.best_sentence(['This is a test',
+        'This is another test',
+        'This is a great test',
+      ], %w{test great this})
+    )
+
+    assert_equal('This is a great test',
+      WordPlay.best_sentence(['This is a great test',
+        %q{still the best}
+      ])
+    )
+  end
+
+  def test_basic_pronouns
+    assert_equal('i am a robot', WordPlay.switch_pronouns("you are a robot"))
+    assert_equal("you are a person", WordPlay.switch_pronouns("i am a person"))
+    assert_equal("i love you", WordPlay.switch_pronouns("you love me"))
+  end
+
+  def mixed_pronouns
+    assert_equal("you gave me life", WordPlay.switch_pronouns("i gave you life"))
+    assert_equal("i am not what you are", WordPlay.switch_pronouns("you are not what i am"))
+    assert_equal("i annoy your dog", WordPlay.switch_pronouns("you annoy my dog"))
+  end
+
+  def test_complex_pronouns
+    assert_equal("yes, i rule", WordPlay.switch_pronouns("yes, you rule"))
+    assert_equal("why do i cry", WordPlay.switch_pronouns("why do you cry"))
+  end
+end
