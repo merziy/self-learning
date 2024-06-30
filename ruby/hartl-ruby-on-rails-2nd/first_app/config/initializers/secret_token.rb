5787531d7da7f2f243db91b144ed76db6a2abfcf
Alexander Mathis <alexzandermathis@gmail.com>
2024-06-30 06:12:42 -0400

    Init app, Docker edits

---
+++ ruby/hartl-ruby-on-rails-2nd/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '28710f04aff09d1ec6bc5d59d71aef399e213107c6a81d7d2ad09b26763b2b384fe6c435745cf3567bfb2e9b2c9f1b4964b6d93a21b5fe4498633ada30268ce5'
